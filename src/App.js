d244ae2cf76f34c854261b711c628e38d1dfeb58
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-09-07 21:32:12 -0300

    React Router Dom initial configuration performed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
