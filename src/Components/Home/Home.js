16e59f8e7362f8b1bf1ebff7552ad1e7f8637049
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-09-09 04:19:26 -0300

    Added validations to home screen fields

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,22 +9,30 @@ import styles from './Home.module.css';
 const Home = () => {
   const useDefaultName = ['Sim', 'Não'];
   const [apiCategories, setApiCategories] = React.useState([]);
+  const [totalOfJokes, setTotalOfJokes] = React.useState(1);
 
   const [defaultName, setDefaultName] = React.useState('Sim');
   const [name, setName] = React.useState('');
   const [lastName, setLastName] = React.useState('');
+  const [errorFullName, setErrorFullName] = React.useState(false);
   const [numberOfJokes, setNumberOfJokes] = React.useState(1);
+  const [errorNumberOfJokes, setErrorNumberOfJokes] = React.useState(false);
   const [categories, setCategories] = React.useState([]);
+  const [errorCategories, setErrorCategories] = React.useState(false);
   const [jokes, setJokes] = React.useState([]);
 
   const pullJokes = async () => {
+    const fullName = `?firstName=${name}&lastName=${lastName}`;
+
     let responses = [];
     const fetchs = [];
     for (let i = 0; i < numberOfJokes; i += 1) {
       responses.push(i);
       fetchs.push(
         await pull(
-          `http://api.icndb.com/jokes/random?limitTo=[${[...categories]}]`,
+          `http://api.icndb.com/jokes/random?escape=javascript?limitTo=[${[
+            ...categories,
+          ]}]${defaultName === 'Não' ? fullName : ''}`,
         ),
       );
       responses = await Promise.all(fetchs);
@@ -33,9 +41,33 @@ const Home = () => {
     setJokes(responses);
   };
 
+  const validateFields = () => {
+    const validateDefaultName = defaultName === 'Não';
+    const validateFullName = name.length < 3 || lastName.length < 3;
+    const validateNumber = numberOfJokes < 1 || numberOfJokes > totalOfJokes;
+    const validateCategories = !categories.length;
+
+    if (validateDefaultName)
+      validateFullName ? setErrorFullName(true) : setErrorFullName(false);
+
+    validateNumber ? setErrorNumberOfJokes(true) : setErrorNumberOfJokes(false);
+
+    validateCategories ? setErrorCategories(true) : setErrorCategories(false);
+
+    if (!validateDefaultName) {
+      if (!validateNumber && !validateCategories) pullJokes();
+    } else if (validateDefaultName) {
+      if (!validateFullName && !validateNumber && !validateCategories)
+        pullJokes();
+    }
+  };
+
   React.useEffect(async () => {
     const jsonCategories = await pull('http://api.icndb.com/categories');
     setApiCategories([...jsonCategories.value]);
+
+    const jsonTotalOfJokes = await pull('http://api.icndb.com/jokes/count');
+    setTotalOfJokes(jsonTotalOfJokes.value);
   }, []);
 
   return (
@@ -67,15 +99,28 @@ const Home = () => {
                 setValue={setLastName}
               />
             </div>
+            {errorFullName && (
+              <p className={styles.error}>
+                Por favor, digite mais de 3 caracteres em seu nome e sobrenome
+              </p>
+            )}
           </div>
         )}
 
-        <InputForm
-          label="Número de piadas"
-          type="number"
-          value={numberOfJokes}
-          setValue={setNumberOfJokes}
-        />
+        <div className={styles.numberOfJokes}>
+          <InputForm
+            label="Número de piadas"
+            type="number"
+            value={numberOfJokes}
+            setValue={setNumberOfJokes}
+          />
+          {errorNumberOfJokes && (
+            <p className={styles.error}>
+              Por favor, o número de piadas não pode ser menor que 1 ou maior
+              que {totalOfJokes}
+            </p>
+          )}
+        </div>
 
         <div>
           <CheckboxForm
@@ -84,13 +129,18 @@ const Home = () => {
             categories={categories}
             setCategories={setCategories}
           />
+          {errorCategories && (
+            <p className={styles.error}>
+              Por favor, selecione pelo menos uma categoria
+            </p>
+          )}
         </div>
 
         <Button
           variant="contained"
           color="primary"
           size="large"
-          onClick={pullJokes}
+          onClick={validateFields}
         >
           GERAR PIADAS
         </Button>
